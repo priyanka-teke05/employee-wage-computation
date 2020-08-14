3c256496333c152227821e3651172fae44000b5a
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-14 21:44:57 +0530

    ADDED: calculate employee wage according to parttime and fulltime

--- employeeWage.sh
+++ employeeWage.sh
@@ -5,15 +5,22 @@ echo "Welcome to Employee Wage Computation"
 
 #constants
 readonly IS_EMP_PRESENT=1
+readonly IS_PART_TIME=1
+readonly IS_FULL_TIME=2
+readonly EMP_RATE_PER_HRS=20
 
 #random to get 0 or 1
 empAttendance=$(( RANDOM%2 ))
 
 #check employee is present or absent and calculate salary
 if [ $IS_EMP_PRESENT -eq $empAttendance ]; then
-	empRatePerHrs=20 
-	empHours=8 
-	salary=$((empHours*empRatePerHrs))
+	empCheck=$(( RANDOM%3 ))
+	if [ $empCheck -eq $IS_PART_TIME ]; then
+		empHours=4
+	elif [ $empCheck -eq $IS_FULL_TIME ]; then
+		empHours=8
+	fi
 else
-	salary=0
+	empHours=0
 fi
+salary=$(( empHours*EMP_RATE_PER_HRS ))
