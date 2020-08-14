1839ed5dd395ceb244615f16d1ae794904640495
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-14 22:48:44 +0530

    ADDED: create function to get employee hours

--- employeeWage.sh
+++ employeeWage.sh
@@ -14,25 +14,29 @@ readonly TOTAL_WORKING_HRS=100
 totalSalary=0
 empWorkingHours=0
 
+#function get employee working hours
+function getWorkingHours()
+{
+	local empCheck=$1
+	#assign employee hours according to employee check
+   case $empCheck in
+      $IS_PART_TIME)
+         empHours=4
+         ;;
+      $IS_FULL_TIME)
+         empHours=8
+         ;;
+      *)
+         empHours=0
+         ;;
+   esac
+	echo $empHours
+}
+
 while [[ $empWorkingHours -lt $TOTAL_WORKING_HRS && $dayCount -lt $NUM_OF_WORKING_DAYS ]]
 do
 	((dayCount++))
-
-	#random to get 1 or 2
-	empCheck=$(( RANDOM%3 ))
-
-	#assign employee hours according to employee check
-	case $empCheck in
-		$IS_PART_TIME)
-			empHours=4
-			;;
-		$IS_FULL_TIME)
-			empHours=8
-			;;
-		*)
-			empHours=0
-			;;
-	esac
+	empHours="$( getWorkingHours $((RANDOM%3)) )"
 	empWorkingHours=$((empWorkingHours+empHours))
 done
 
