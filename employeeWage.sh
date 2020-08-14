df17989383cea62accf3a95a01628a9def3f5edf
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-14 21:34:06 +0530

    ADDED: calculate employee's daily wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -9,9 +9,11 @@ readonly IS_EMP_PRESENT=1
 #random to get 0 or 1
 empAttendance=$(( RANDOM%2 ))
 
-#check employee is present or absent
+#check employee is present or absent and calculate salary
 if [ $IS_EMP_PRESENT -eq $empAttendance ]; then
-	echo "Employee is present"
+	empRatePerHrs=20 
+	empHours=8 
+	salary=$((empHours*empRatePerHrs))
 else
-	echo "Employee is absent"
+	salary=0
 fi
