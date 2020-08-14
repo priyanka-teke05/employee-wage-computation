25481004f402fb81b0194391e7f46639d5f8f5fa
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-14 22:05:09 +0530

    ADDED: calculate employee wage for month

--- employeeWage.sh
+++ employeeWage.sh
@@ -7,22 +7,28 @@ echo "Welcome to Employee Wage Computation"
 readonly IS_PART_TIME=1
 readonly IS_FULL_TIME=2
 readonly EMP_RATE_PER_HRS=20
+readonly NUM_OF_WORKING_DAYS=20
 
-#random to get 1 or 2
-empCheck=$(( RANDOM%3 ))
+#variables
+totalSalary=0
 
-#assign employee hours according to employee check
-case $empCheck in
-	$IS_PART_TIME)
-		empHours=4
-		;;
-	$IS_FULL_TIME)
-		empHours=8
-		;;
-	*)
-		empHours=0
-		;;
-esac
+for(( dayCount=1; dayCount<=NUM_OF_WORKING_DAYS; dayCount++ ))
+do
+	#random to get 1 or 2
+	empCheck=$(( RANDOM%3 ))
 
-#calculate salary
-salary=$(( empHours*EMP_RATE_PER_HRS ))
+	#assign employee hours according to employee check
+	case $empCheck in
+		$IS_PART_TIME)
+			empHours=4
+			;;
+		$IS_FULL_TIME)
+			empHours=8
+			;;
+		*)
+			empHours=0
+			;;
+	esac
+	perDaySalary=$(( $empHours*EMP_RATE_PER_HRS ))
+	totalSalary=$((totalSalary+perDaySalary))
+done
